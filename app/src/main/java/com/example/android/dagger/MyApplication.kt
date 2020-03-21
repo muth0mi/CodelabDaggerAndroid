ca8ea7a499b0b2cdd17726b806c5928f66a69933
Oliver Muthomi <oliver.mwitia@gmail.com>
2020-03-22 01:35:45 +0300

    Testing with Dagger

--- app/src/main/java/com/example/android/dagger/MyApplication.kt
+++ app/src/main/java/com/example/android/dagger/MyApplication.kt
@@ -24,10 +24,11 @@ import com.example.android.dagger.user.UserManager
 
 open class MyApplication : Application() {
 
-    // Instance of the AppComponent that will be used by all the Activities in the project
     val appComponent: AppComponent by lazy {
-        // Creates an instance of AppComponent using its Factory constructor
-        // We pass the applicationContext that will be used as Context in the graph
-        DaggerAppComponent.factory().create(applicationContext)
+        initializeComponent()
+    }
+
+    open fun initializeComponent(): AppComponent {
+        return DaggerAppComponent.factory().create(applicationContext)
     }
 }
